accc3b60c27e1974675618a358f5d1251fa0efd7
Sawyer Aviram <aviram.sawyer@gmail.com>
2021-03-06 16:53:43 -0500

    added play method and deal_cards pieces but we are using other code

--- game.rb
+++ game.rb
@@ -27,11 +27,26 @@ $game_deck.shuffle
 class Game
 
   def deal_cards
-     $person.hand.push($game_deck.deck.first(2))
-     $computer.hand.push($game_deck.deck.last(2))
-    p $person.hand[0][0]
+    p $person.hand.push($game_deck.deck.first(2))
+    $computer.hand.push($game_deck.deck.last(2))
+    $player_hand = $person.hand[0][0].num + $person.hand[0][1].num
+    $computer_hand = $computer.hand[0][0].num + $computer.hand[0][1].num
+    p $player_hand
+    p $computer_hand
     # p $player_hand = $person.hand[0][0] + $person.hand[1][@num].to_i
     # p $computer_hand = $computer.hand[0][@num].to_i + $computer.hand[1][@num].to_i
+  end
+
+  def play
+    if $player_hand < 21 && $player_hand > $computer_hand
+      player_bankroll_update = $person.bankroll + 10
+      computer_bankroll_update = $computer.bankroll - 10
+      p `The player has won, you now have #{player_bankroll_update}`
+    elsif $computer_hand < 21 && $computer_hand > $player_hand
+      player_bankroll_update = $person.bankroll - 10
+      computer_bankroll_update = $computer.bankroll + 10
+      p `The player has loss, you now have #{player_bankroll_update}`
+    end
   end
 
 end
